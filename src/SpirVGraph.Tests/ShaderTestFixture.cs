709f78abdde9c6c998a2dd0e85af66894d168aee
Gleb <gleb@gleblebedev.com>
2019-07-21 10:36:40 +0100

    copy paste removed

--- src/SpirVGraph.Tests/ShaderTestFixture.cs
+++ src/SpirVGraph.Tests/ShaderTestFixture.cs
@@ -74,40 +74,5 @@ void main()
             }
             Assert.Fail("Didn't find float constant in the shader code:\n" + shader);
         }
-
-
-        [Test]
-        public void FloatConstant_ParsedCorrectly()
-        {
-            var res = SpirvCompilation.CompileGlslToSpirv(@"
-#version 450
-
-
-
-void main()
-{
-    float x = 12.3;
-    gl_Position = vec4(x);
-}
-", "shader.vert", ShaderStages.Vertex, GlslCompileOptions.Default);
-            var shader = SpirVGraph.Shader.Parse(res.SpirvBytes);
-#if DEBUG
-            Console.WriteLine(shader);
-#endif
-
-            var constants = shader.Instructions.Where(_ => _.OpCode == Op.OpConstant).Select(_ => (OpConstant)_).ToList();
-            foreach (var instruction in constants)
-            {
-                if (instruction.Value.Value.Type.OpCode == Op.OpTypeFloat &&
-                    instruction.Value.Value.Type.SizeInWords == 1)
-                {
-                    if (instruction.Value.Value.ToFloat() == 12.3f)
-                    {
-                        return;
-                    }
-                }
-            }
-            Assert.Fail("Didn't find float constant in the shader code:\n" + shader);
-        }
     }
 }
